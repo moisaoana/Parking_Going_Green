b1c05ac502fbad85ec074fbe46b04ce1206178a0
Ileana Pop <ileanapop1811@gmail.com>
2021-11-07 19:39:29 +0200

    add login page config

--- src/main/java/com/example/seproject/SeProjectApplication.java
+++ src/main/java/com/example/seproject/SeProjectApplication.java
@@ -3,19 +3,26 @@ package com.example.seproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
+import org.springframework.web.servlet.ModelAndView;
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@EnableCaching
 public class SeProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SeProjectApplication.class, args);
     }
-    @GetMapping("/hello") public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-       return String.format("Hello %s!", name); }
+    /*@GetMapping("/hello") public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+       return String.format("Hello %s!", name); }*/
+
+   /* @RequestMapping("/")
+    public String index(){
+        return "Login";
+    }*/
 
 }
