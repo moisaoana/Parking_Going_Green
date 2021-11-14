a618e442e145d789092753f82e22ccac59f01d20
ituanca <itu_anca@yahoo.com>
2021-11-14 12:07:31 +0200

    commit hello world

--- src/main/java/com/example/seproject/HelloWorldController.java
+++ src/main/java/com/example/seproject/HelloWorldController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class LoginController {
-
-    @RequestMapping("/login")
+public class HelloWorldController {
+    @RequestMapping("/")
     public String index(){
-        return "login";
+        return "helloWorld";
     }
 }
