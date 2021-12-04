2dde86be160f55c0f669f21a1a5c988a218edf9c
Ileana Pop <ileanapop1811@gmail.com>
2021-12-04 13:19:45 +0200

    create user functionality + design profile page

--- src/main/java/com/example/seproject/Controller/LoginController.java
+++ src/main/java/com/example/seproject/Controller/LoginController.java
@@ -1,13 +1,36 @@
 package com.example.seproject.Controller;
 
+import com.example.seproject.Model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String index(){
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("user", new User());
         return "login";
     }
+
+
+    @PostMapping("/profile")
+    public String greetingSubmit(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        return "profile";
+    }
+
+    /*@PostMapping("/login")
+    public String greetingSubmit1(@ModelAttribute User user, Model model) {
+        model.addAttribute("login", user);
+        System.out.println(user.getUsername());
+        return "result";
+    }*/
+
+
 }
--- src/main/java/com/example/seproject/Model/User.java
+++ src/main/java/com/example/seproject/Model/User.java
@@ -1,9 +1,42 @@
 package com.example.seproject.Model;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 public class User {
     private String username;
     private String password;
     private Zone currentZone;
     private Zone destinationZone;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Zone getCurrentZone() {
+        return currentZone;
+    }
+
+    public void setCurrentZone(Zone currentZone) {
+        this.currentZone = currentZone;
+    }
+
+    public Zone getDestinationZone() {
+        return destinationZone;
+    }
+
+    public void setDestinationZone(Zone destinationZone) {
+        this.destinationZone = destinationZone;
+    }
 }
